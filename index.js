613bffad73334c3c3292cdf99f54daf7b31b5a19
Ashley Muller <serenehopes@gmail.com>
2018-12-20 00:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Neighborhood{
-  
+  constructor(){
+    
+  }
 }
 
 class Meal{
+  constructor(){
+    
+  }
   
 }
 
 class Customer{
+constructor(){
   
+}
 }
 
 class Delivery{
-  
+  constructor(){
+    
+  }
 }

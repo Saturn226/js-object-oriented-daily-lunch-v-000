3c9088ece8259559894177ac732e68ee3e650e8f
Ashley Muller <serenehopes@gmail.com>
2018-12-20 00:25:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Neighborhood{
+  let neighborhoodId= 0;
  constructor(name){
    this.name = name
+   this.id = neighborhoodId++ 
  } 
 }
 
